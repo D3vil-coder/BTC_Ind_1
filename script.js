d213cd9d46691440ae421ca14e23229eb3e74b12
D3vil-coder <whitedevilxtra@gmail.com>
2024-10-06 05:44:33 +0530

    Add files via upload
    First draft of the Temporary BTC India site

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("JoinUs").onclick = function () {
+    location.href = "https://t.me/btcindia_org";
+};
+
+document.getElementById("ContUs").onclick = function () {
+    location.href = "https://mail.google.com/mail/u/0/?fs=1&to=contact@btc-india.org&tf=cm";
+};
+
+document.getElementById("Follow").onclick = function () {
+    location.href = "https://x.com/btcindia_org";
+};
